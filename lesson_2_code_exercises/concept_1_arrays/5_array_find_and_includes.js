082fd64321efb493f247db8a30cdf0a880d7aec9
berkeleycole <alyssamhope@gmail.com>
2019-12-18 18:07:32 -0800

    more changes

--- lesson_2_code_exercises/concept_1_arrays/5_array_find_and_includes.js
+++ lesson_2_code_exercises/concept_1_arrays/5_array_find_and_includes.js
@@ -72,3 +72,45 @@ const ids = [
     'ABCKO',
     'IUABC'
 ]
+
+
+// ----------------------------------------------------------
+
+// It best to use INCLUDES when what the value is does not matter, simply its presence. Imagine the scenario that you are need to check a user's id against a list of admin id's.
+
+const currentUserId = '29nv283bfc0szn16723'
+
+const admins = [
+    '02398cn7syap0dmbnv0',
+    '2389sakvjhw8e7f09fv',
+    '09mxvb82kzjd6v1sfdg',
+    '9a76zxmsdnv1u622345',
+    '29nv283bfc0szn16723',
+    '029834zmnv9jhgfu2ab',
+    '12mnz09v87bas78fb12',
+    '098Xc8x76m3nb4aposi'
+]
+
+// ----------------------------------------------------------
+
+// Checking between lists. Another thing that you might need to do in real life is check items between two arrays. Check if array B has any values that are also in array A. This becomes especially helpful when the values are hard to distinguish visually
+
+const A = [
+    '02398cn7syap0dmbnv0',
+    '2389sakvjhw8e7f09fv',
+    '09mxvb82kzjd6v1sfdg',
+    '9a76zxmsdnv1u622345',
+    '29nv283bfc0szn16723',
+    '029834zmnv9jhgfu2ab',
+    '12mnz09v87bas78fb12',
+    '098Xc8x76m3nb4aposi'
+]
+
+const B = [
+    '13xnse8aanv87Hdnfv8',
+    '2389sakvjhw8e7f09fv',
+    '12mn0vnZkadfh237LPd',
+    '1209MNBd8723nvkwejs',
+    '298374naskdj273ubsl',
+    '098LKJnsvijevkwejf6'
+]
--- lesson_3_code_exercises/1_higher_order_functions_practice.js
+++ lesson_3_code_exercises/1_higher_order_functions_practice.js
@@ -3,3 +3,35 @@
 // HIGHER ORDER FUNCTIONS
 
 // ----------------------------------------------------------
+
+const weapons = ['Phaser', 'Blaster', 'Sonic Screwdriver', 'Lightsaber', 'Noisy Cricket']
+let arsenal = []
+// have to use let because this value will change
+
+function add_to_arsenal(weapon) {
+    arsenal = arsenal.concat(weapon)
+    // update arsenal
+}
+
+weapons.map(add_to_arsenal)
+
+// ALTERNATE SYNTAX
+// weapons.map(w => add_to_arsenal(w))
+
+console.log(arsenal)
+
+function greaterThan(n) {
+  return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+
+
+weapons_with_noises = [
+    {weapon: 'Phaser', noise: 'Pew Pew'},
+    {weapon: 'Blaster', noise: 'Pew Pew'},
+    {weapon: 'Sonic Screwdriver', noise: 'Pew Pew'},
+    {weapon: 'Lightsaber', noise: 'Pew Pew'},
+    {weapon: 'Noisy Cricket', noise: 'Pew Pew'}
+]
