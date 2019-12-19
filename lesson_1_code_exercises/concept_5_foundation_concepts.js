f9253edff3e2f2c9a0d11dd43b2358cab2723de3
berkeleycole <alyssamhope@gmail.com>
2019-12-18 19:30:29 -0800

    pure functions practice

---
+++ lesson_1_code_exercises/concept_5_foundation_concepts.js
@@ -0,0 +1,22 @@
+// Translate from normal function to pure function
+let greeting = 'Hello, '
+
+const greet = (name) => {
+    return greeting += name
+}
+
+// this code could be problematic because greeting is in the global scope. 
+// We could not use the greeting a second time, because the greeting variable is changed to contain the name
+
+
+// Solution
+
+const greet = (name, base_greeting) => {
+    return `${base_greeting} ${name}`
+}
+
+let greeting = "Hello, "
+
+greet('Arthur', greeting)
+
+// now we can re-use greeting, and the greet function is pure
