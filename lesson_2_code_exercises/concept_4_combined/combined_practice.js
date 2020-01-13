0139842227a724f50e9a8b078591ea6169830a8f
alyssamhope <berkeleycole@users.noreply.github.com>
2020-01-13 10:10:43 -0800

    Update combined_practice.js

--- lesson_2_code_exercises/concept_4_combined/combined_practice.js
+++ lesson_2_code_exercises/concept_4_combined/combined_practice.js
@@ -1,10 +1,109 @@
+let characters = [
+  {
+    name: 'Marvin the Paranoid Android',
+    role: 'First Mate',
+    universe: 'Hitchhikers Guide to the Galaxy',
+    weapon: 'severe depression',
+    power_level: 1000
+  },
+  {
+    name: 'Jabba the Hut',
+    role: 'villain',
+    universe: 'Star Wars',
+    weapon: 'henchmen',
+    power_level: 200
+  },
+  {
+    name: 'Zoë Alleyne Washburne',
+    role: 'First Mate',
+    universe: 'Firefly',
+    weapon: 'Winchester Model 1892 rifle',
+    power_level: 160
+  },
+  {
+    name: 'Peter Venkman',
+    role: 'Ghostbuster',
+    universe: 'Ghostbusters',
+    weapon: 'proton pack',
+    power_level: 120
+  },
+  {
+    name: 'Kathryn Janeway',
+    role: 'Captain',
+    universe: 'Star Trek',
+    weapon: 'Wit',
+    power_level: 140
+  },
+  {
+    name: 'Dr. Daniel Jackson',
+    role: 'Archeologist',
+    universe: 'Stargate',
+    weapon: 'Zat gun',
+    power_level: 120
+  },
+  {
+    name: 'Q',
+    role: 'God/Eternal',
+    universe: 'Star Trek',
+    weapon: 'Whatever he wants',
+    power_level: 1000
+  },
+  {
+    name: 'Boba Fett',
+    role: 'Bounty Hunter',
+    universe: 'Star Wars',
+    weapon: 'EE-3 carbine rifle',
+    power_level: 400
+  },
+  {
+    name: 'Yoda',
+    role: 'Jedi Master',
+    universe: 'Star Wars',
+    weapon: 'The Force',
+    power_level: 900
+  },
+  {
+    name: 'Mal Reynolds',
+    role: 'Captain',
+    universe: 'Firefly',
+    weapon: 'pistol',
+    power_level 160
+  },
+  {
+    name: 'Spock',
+    role: 'First Mate',
+    universe: 'Star Trek',
+    weapon: 'Logic',
+    power_level: 170
+  },
+  {
+    name: 'R2-D2',
+    role: 'Ship`s Robot',
+    universe: 'Star Wars',
+    weapon: 'Data Probe',
+    power_level: 250
+  },
+  {
+    name: 'Lore',
+    role: 'Villain',
+    universe: 'Star Trek',
+    weapon: 'Intellect',
+    power_level: 800
+  },
+]
+
 // ----------------------------------------------------------
 
 // COMBINED PRACTICE 1
 
 // ----------------------------------------------------------
 
-// Given these values, find 
+// Create an array containing only the names of Captains from all universes. 
+
+// expected output:
+
+// solution:
+
 
 // ----------------------------------------------------------
 
@@ -12,14 +111,33 @@
 
 // ----------------------------------------------------------
 
+// Create 
+
+// expected output:
+
+// solution:
+
 // ----------------------------------------------------------
 
 // COMBINED PRACTICE 3
 
 // ----------------------------------------------------------
 
+// Create an array containing any characters who are the only character listed in their universe.
+
+// expected output:
+
+// solution:
+
+
 // ----------------------------------------------------------
 
 // COMBINED PRACTICE 4
 
 // ----------------------------------------------------------
+
+//
+
+// expected output:
+
+// solution:
