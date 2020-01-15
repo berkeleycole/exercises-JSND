a8474a7382edaf84b9e8e0ad9e559ffbcae5eba9
alyssamhope <berkeleycole@users.noreply.github.com>
2020-01-14 21:34:37 -0800

    Update combined_practice.js

--- lesson_2_code_exercises/concept_4_combined/combined_practice.js
+++ lesson_2_code_exercises/concept_4_combined/combined_practice.js
@@ -100,10 +100,10 @@ const characters = [
 
 // Create an array containing only the names of Captains from all universes.
 
-// expected output:
+// expected output: 
 
 // solution:
-const captainNames = characters.filter(c => c.role == 'Captain').join(', ')
+// const captainNames = characters.filter(c => c.role == 'Captain').join(', ')
 
 
 // ----------------------------------------------------------
@@ -116,7 +116,85 @@ const captainNames = characters.filter(c => c.role == 'Captain').join(', ')
 
 // expected output:
 
+// [ [ { name: 'Marvin the Paranoid Android',
+//       role: 'First Mate',
+//       universe: 'Hitchhikers Guide to the Galaxy',
+//       weapon: 'severe depression',
+//       power_level: 1000 } ],
+//   [ { name: 'Jabba the Hut',
+//       role: 'villain',
+//       universe: 'Star Wars',
+//       weapon: 'henchmen',
+//       power_level: 200 },
+//     { name: 'Boba Fett',
+//       role: 'Bounty Hunter',
+//       universe: 'Star Wars',
+//       weapon: 'EE-3 carbine rifle',
+//       power_level: 400 },
+//     { name: 'Yoda',
+//       role: 'Jedi Master',
+//       universe: 'Star Wars',
+//       weapon: 'The Force',
+//       power_level: 900 },
+//     { name: 'R2-D2',
+//       role: 'Ship`s Robot',
+//       universe: 'Star Wars',
+//       weapon: 'Data Probe',
+//       power_level: 250 } ],
+//   [ { name: 'Zoë Alleyne Washburne',
+//       role: 'First Mate',
+//       universe: 'Firefly',
+//       weapon: 'Winchester Model 1892 rifle',
+//       power_level: 160 },
+//     { name: 'Mal Reynolds',
+//       role: 'Captain',
+//       universe: 'Firefly',
+//       weapon: 'pistol',
+//       power_level: 160 } ],
+//   [ { name: 'Peter Venkman',
+//       role: 'Ghostbuster',
+//       universe: 'Ghostbusters',
+//       weapon: 'proton pack',
+//       power_level: 120 } ],
+//   [ { name: 'Kathryn Janeway',
+//       role: 'Captain',
+//       universe: 'Star Trek',
+//       weapon: 'Wit',
+//       power_level: 140 },
+//     { name: 'Q',
+//       role: 'God/Eternal',
+//       universe: 'Star Trek',
+//       weapon: 'Whatever he wants',
+//       power_level: 1000 },
+//     { name: 'Spock',
+//       role: 'First Mate',
+//       universe: 'Star Trek',
+//       weapon: 'Logic',
+//       power_level: 170 },
+//     { name: 'Lore',
+//       role: 'Villain',
+//       universe: 'Star Trek',
+//       weapon: 'Intellect',
+//       power_level: 800 } ],
+//   [ { name: 'Dr. Daniel Jackson',
+//       role: 'Archeologist',
+//       universe: 'Stargate',
+//       weapon: 'Zat gun',
+//       power_level: 120 } ] ]
+
 // solution: 
+
+// const groupedCharacters = characters
+//     .reduce((acc, curr, i, arr) => {
+//         acc[curr.universe] = acc[curr.universe] === undefined ? [] : acc[curr.universe]
+//         acc[curr.universe].push(curr)
+
+//         if (i + 1 == arr.length) {
+//             return Object.values(acc)
+//         }
+
+//         return acc
+//  }, {})
 
 // ----------------------------------------------------------
 
@@ -126,28 +204,28 @@ const captainNames = characters.filter(c => c.role == 'Captain').join(', ')
 
 // Create an array containing characters' names who are the only character listed in their universe.
 
-// expected output:
+// expected output: [ Marvin the Paranoid Android, Peter Venkman, Dr. Daniel Jackson ]
 
 // solution:
-const groupByUniverse = (acc, curr, i, arr) => {
-    acc[curr.universe] = acc[curr.universe] === undefined ? [] : acc[curr.universe]
-    acc[curr.universe].push(curr)
+// const groupByUniverse = (acc, curr, i, arr) => {
+//     acc[curr.universe] = acc[curr.universe] === undefined ? [] : acc[curr.universe]
+//     acc[curr.universe].push(curr)
 
-    if (i + 1 == arr.length) {
-        return Object.entries(acc)
-            .filter(([_, characters]) => characters.length === 1)
-            .map(([_, characters]) => characters[0])
-    }
+//     if (i + 1 == arr.length) {
+//         return Object.entries(acc)
+//             .filter(([_, characters]) => characters.length === 1)
+//             .map(([_, characters]) => characters[0])
+//     }
 
-    return acc
-}
+//     return acc
+// }
 
-const soloCharacters = characters
-    .reduce(groupByUniverse, {})
-    .map(character => character.name)
-    .join(', ')
+// const soloCharacters = characters
+//     .reduce(groupByUniverse, {})
+//     .map(character => character.name)
+//     .join(', ')
 
-console.log('soloCharacters:', soloCharacters)
+// console.log('soloCharacters:', soloCharacters)
 
 // ----------------------------------------------------------
 
@@ -156,3 +234,12 @@ console.log('soloCharacters:', soloCharacters)
 // ----------------------------------------------------------
 
 // What is the average power level across all characters?
+
+// expected output: 68.71319452795147
+
+// solution:
+// const avgPowerLvl = characters
+//     .map(c => c.power_level)
+//     .reduce((acc, curr, i) => (acc += curr) / i)
+
+// console.log('avgPowerLvl:', avgPowerLvl)
