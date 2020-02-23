3103aa53f6d6dd5e1b93bded1fadbdc3b6d17385
alyssamhope <berkeleycole@users.noreply.github.com>
2020-02-22 16:14:40 -0800

    Update 1_object_freeze.js

--- lesson_2_code_exercises/concept_3_objects/1_object_freeze.js
+++ lesson_2_code_exercises/concept_3_objects/1_object_freeze.js
@@ -4,6 +4,12 @@
 
 // ----------------------------------------------------------
 
+// Note here that we must run Javascript in strict mode in order to see the error correctly
+// Node runs in strict mode automatically, as do many online code editors, so if you are running these examples in the terminal or online you probably won't need this
+// If you want to learn more about strict mode, here is a good article: https://www.geeksforgeeks.org/strict-mode-javascript/
+
+"use strict";
+
 const current_show = {
     title: 'Dr. Who',
     seasons: 11,
@@ -18,6 +24,8 @@ Object.freeze(current_show);
 
 obj.current_season = 5;
 // Throws an error and current_season remains unchanged
+// Note here that in some environments, no error was thrown and it just failed silently - so watch out!
+// We will explore another (and more reliable) way to get immutable values in Javascript in the next lesson
 
 
 
